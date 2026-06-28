3fd4ca62142937526cda2d7b94a12025c515da12
vanshsoni4 <vanshsoni104@gmail.com>
2026-06-28 23:15:04 +0530

    Use environment variable for database

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,8 @@
+import os
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DATABASE_URL = "postgresql://postgres:vansh1914@localhost:5432/goalos"
+DATABASE_URL = os.getenv("DATABASE_URL")
 
 engine = create_engine(DATABASE_URL)
 
